4d513be0b6c8263b776930f5cc5ec31211340542
bk123477 <bk123477@naver.com>
2022-05-18 23:42:56 +0900

    경로 변경에 의한 오류 수정

--- running_app/app/src/main/java/com/example/running_app/MainActivity.java
+++ running_app/app/src/main/java/com/example/running_app/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 1500);
     }
     protected void onPause(){
         super.onPause();
