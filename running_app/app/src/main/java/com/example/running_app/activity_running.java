9a9c45a39b541dc9cf0068269c9c69c870d4e888
bk123477 <bk123477@naver.com>
2022-05-29 17:27:14 +0900

    [FEAT] 달린 거리, 달린 시간 저장 완료(API작성 해야함)

--- running_app/app/src/main/java/com/example/running_app/activity_running.java
+++ running_app/app/src/main/java/com/example/running_app/activity_running.java
@@ -1,9 +1,11 @@
 package com.example.running_app;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationRequest;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -13,6 +15,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationRequest;
+import android.location.LocationManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +27,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -51,7 +61,11 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
     private int cnt = 1;
 
     //타이머 시간 값을 저장할 변수
-    private long baseTime,pauseTime;
+    private long baseTime, pauseTime;
+    private static double runDistance = 0.0;
+    private static double beforeLat = 0.0;
+    private static double beforeLon = 0.0;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstaceState) {
@@ -60,6 +74,21 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.running);
         mapFragment.getMapAsync(this);
+
+
+        //거리 초기화
+        runDistance = 0.0;
+
+        //거리 구하는 공식은 이런식으로 하면 될듯..
+//        Location locA = new Location("point A");
+//        locA.setLatitude(37.5582876);
+//        locA.setLongitude(127.0001671);
+//        Location locB = new Location("point B");
+//        locB.setLatitude(37.557442);
+//        locB.setLongitude(127.001449);
+//
+//        double distance = locA.distanceTo(locB);
+//        Toast.makeText(activity_running.this, ""+distance, Toast.LENGTH_SHORT).show();
 
 
         // 하단 버튼 구성
@@ -79,7 +108,7 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
         home_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),activity_home.class);
+                Intent intent = new Intent(getApplicationContext(), activity_home.class);
                 startActivity(intent);
                 finish();
             }
@@ -97,7 +126,7 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
         });
 
         //객체화
-        runningTime = (TextView)findViewById(R.id.time);
+        runningTime = (TextView) findViewById(R.id.time);
         start_btn = findViewById(R.id.runningStart);
         end_btn = findViewById(R.id.runningEnd);
         // runningDistance = findViewById(R.id.distance);
@@ -109,9 +138,10 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.runningStart :
+            switch (v.getId()) {
+                case R.id.runningStart:
                     start_btn.setText("일시 중단");
+                    startLocationService();
                     StaButton();
             }
         }
@@ -130,8 +160,8 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
     };
 
 
-    private void StaButton(){
-        switch(status) {
+    private void StaButton() {
+        switch (status) {
             case INIT:
                 //어플리케이션이 실행되고 나서 실제로 경과된 시간
                 baseTime = SystemClock.elapsedRealtime();
@@ -167,18 +197,17 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
         }
     }
 
-    private String getTime(){
+    private String getTime() {
         //경과된 시간 체크
-
         long nowTime = SystemClock.elapsedRealtime();
         //시스템이 부팅된 이후의 시간?
         long overTime = nowTime - baseTime;
 
-        long h = overTime/1000/3600;
-        long m = overTime/1000/60;
-        long s = (overTime/1000)%60;
+        long h = overTime / 1000 / 3600;
+        long m = overTime / 1000 / 60;
+        long s = (overTime / 1000) % 60;
 
-        String recTime = String.format("%02d:%02d:%02d", h, m,s);
+        String recTime = String.format("%02d:%02d:%02d", h, m, s);
 
         return recTime;
     }
@@ -187,7 +216,7 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
         @Override
         public void handleMessage(@NonNull Message msg) {
             runningTime.setText(getTime());
-            if (status == END){
+            if (status == END) {
                 pauseTime = SystemClock.elapsedRealtime();
                 baseTime = SystemClock.elapsedRealtime();
                 runningTime.setText("00:00:00");
@@ -204,18 +233,19 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.googleMap = googleMap;
         //37.5582876,127.0001671 동국대
-        LatLng latLng = new LatLng(37.5582876,127.0001671);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,13));
+        LatLng latLng = new LatLng(37.5582876, 127.0001671);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
         MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("여기");
         markerOptions.snippet("여기엔 간단한 설명");
         googleMap.addMarker(markerOptions);
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             googleMap.setMyLocationEnabled(true);
         } else {
             checkLocationPermissionWithRationale();
         }
 
     }
+
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     private void checkLocationPermissionWithRationale() {
@@ -235,6 +265,7 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
             }
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -249,6 +280,65 @@ public class activity_running extends AppCompatActivity implements OnMapReadyCal
                 }
                 return;
             }
+        }
+    }
+
+    private void startLocationService() {
+        //get manager instance
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        //set listener
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        float minDistance = 0;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime, minDistance, gpsListener);
+    }
+    private class GPSListener implements LocationListener{
+        public void onLocationChanged(Location location){
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            double distance;
+            if(beforeLat == 0.0){
+                beforeLat = latitude;
+            }
+            if(beforeLon == 0.0){
+                beforeLon = longitude;
+            }
+            String msg = "Latitude : " + latitude + "\nLongitude: "+ longitude;
+            Log.i("", msg);
+            Location before = new Location("beforeLat, beforeLon");
+            before.setLatitude(beforeLat);
+            before.setLongitude(beforeLon);
+            Location current = new Location("latitude, longitude");
+            current.setLatitude(latitude);
+            current.setLongitude(longitude);
+            distance = before.distanceTo(current);
+            runDistance += distance;
+            Log.i("누적 거리", ""+runDistance);
+            beforeLat = latitude;
+            beforeLon = longitude;
+        }
+        public void onProviderDisabled(String provider){
+
+        }
+        public void onProviderEnabled(String provider){
+
+        }
+        public void onStatusChanged(String provider, int status, Bundle extras){
+
         }
     }
 }
