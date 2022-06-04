d82c2ae6298f36b2a7ff743454c5233f1f4f5bd2
kwan2 <jgh9987@naver.com>
2022-06-04 15:20:19 +0900

    [MOD] 매칭 완료시 프로필 상에 최신 러닝 정보 데이터 업데이트 쿼리 추가

--- Server/my-server/routes/Result/result.controller.js
+++ Server/my-server/routes/Result/result.controller.js
@@ -1,24 +1,24 @@
 var db = require('../../db/connect');
-
+const preprocessing = require('../../preprocessing/preprocessing');
 const faceinfo= require('./avg_face');
+const mysql = require('mysql');
 /**
  * 러닝 결과 API
  */
 module.exports.input = (req, res) => {
     const conn = db.conn();
-
-    //이거도 아직 스트링임. 러닝탭에서 이 유저의 러닝 코스가 뭔지 표시해 줘야함..
     var course_name = parseInt(req.body.courseNo);
-
     var total_time = req.body.time;
     var total_distance = parseFloat(req.body.distance); //double
     var nickname = req.body.nickname;
     var avg_face = faceinfo(req.body.distance,req.body.time);
     var course_rating = parseFloat(req.body.run_rate);
     var matching_rating = parseFloat(req.body.run_rate);
-
-    var sql1= "INSERT into User_Running_Records (Record_id, nickname, course_name, total_time, avg_face, total_distance,matching_rating,course_rating) value ((SELECT IFNULL(MAX(Record_id)+1,1) FROM  User_Running_Records u), ?, ?, ?, ?, ?,?,?);"
-    var params = [
+    var running_type = preprocessing(total_distance,avg_face);
+    
+    var sql1= 'INSERT into User_Running_Records (Record_id, nickname, course_name, total_time, avg_face, total_distance, matching_rating,course_rating) value ((SELECT IFNULL(MAX(Record_id)+1,1) FROM  User_Running_Records u), ?, ?, ?, ?, ?,?,?);';
+    
+    var params1= [
         nickname,
         course_name,
         total_time,
@@ -28,7 +28,20 @@ module.exports.input = (req, res) => {
         matching_rating,
     ];
 
-    conn.query(sql1, params, function (err, result){
+    let sql1s = mysql.format(sql1,params1);
+
+    var sql2 = ' UPDATE Users SET average_face = ?, running_type =?, running_count = running_count+1, new_run_distance = ?, new_course_no = ? WHERE nickname = ?;';
+
+    var params2 = [
+        avg_face,
+        running_type,
+        total_distance, 
+        course_name,
+        nickname,
+    ]
+    let sql2s = mysql.format(sql2,params2);
+    
+    conn.query( sql1s+sql2s, function (err, result){
         if (err) {
             console.log(err);
             conn.end();
